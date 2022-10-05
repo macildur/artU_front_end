2ea5a8d86648ad096fd56e63783a304626e0ce2b
Brandon Vinh Le <brandonvinhquangle@gmail.com>
2022-10-05 13:36:34 -0600

    Create folder structure

--- ArtU/ArtU/Views/LoginView.swift
+++ ArtU/ArtU/Views/LoginView.swift
@@ -1,26 +1,26 @@
 //
-//  ContentView.swift
+//  LoginView.swift
 //  ArtU
 //
-//  Created by Brandon Vinh Lê on 10/5/22.
+//  Created by Brandon Vinh Le on 10/5/22.
 //
 
 import SwiftUI
 
-struct ContentView: View {
+struct LoginView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hello, World!")
         }
         .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        LoginView()
     }
 }
