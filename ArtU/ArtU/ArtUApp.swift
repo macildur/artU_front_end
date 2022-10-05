6c7f7c16a4604a4607d283c3be8c1a2ac5df1c97
Brandon Vinh Le <brandonvinhquangle@gmail.com>
2022-10-05 13:13:36 -0600

    Start new front end project

---
+++ ArtU/ArtU/ArtUApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtUApp.swift
+//  ArtU
+//
+//  Created by Brandon Vinh Lê on 10/5/22.
+//
+
+import SwiftUI
+
+@main
+struct ArtUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
